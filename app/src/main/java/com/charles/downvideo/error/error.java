fce3df98452e7c7492905f9814865a208c0707fc
17111980 <panlia@cnsuning.com>
2019-10-24 16:49:17 +0800

    删除第一个方法代码

--- app/src/main/java/com/charles/downvideo/error/error.java
+++ app/src/main/java/com/charles/downvideo/error/error.java
@@ -1,7 +1,8 @@
 package com.charles.downvideo.error;
 
 public class error {
-    public void addTest(){
-        System.out.println("添加一个方法");
+
+    public void addNodeal(){
+        System.out.println("添加二个方法");
     }
 }
