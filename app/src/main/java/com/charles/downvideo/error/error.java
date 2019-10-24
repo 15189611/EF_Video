ed5625638459a3101251154422b4be52f6308225
17111980 <panlia@cnsuning.com>
2019-10-24 16:57:50 +0800

    Revert "删除第一个方法代码"
    This reverts commit fce3df98452e7c7492905f9814865a208c0707fc.

--- app/src/main/java/com/charles/downvideo/error/error.java
+++ app/src/main/java/com/charles/downvideo/error/error.java
@@ -1,9 +1,8 @@
 package com.charles.downvideo.error;
 
 public class error {
-
-    public void addNodeal(){
-        System.out.println("添加二个方法");
+    public void addTest(){
+        System.out.println("添加一个方法");
     }
 
 
